8b1444b57eefa8f0ac8387fbbb40564f422e2409
Benedikt-rh <benedikt.m.rauh@fau.de>
2025-03-29 23:49:30 +0100

    Update geometry.cc with linear fit

--- src/efi/source/grid/geometry.cc
+++ src/efi/source/grid/geometry.cc
@@ -375,8 +375,8 @@ ImportedGeometry<dim>::create_triangulation (dealii::Triangulation<dim> &tria)
                         mu_element = 10e-6;
                     } else {
                         // Andernfalls berechne den mu-Wert mit der Formel
-                        mu_element = fa_value;
-                        //mu_element = (-(fa_value / 0.0037) + 182.4)*1e-6;
+                        //mu_element = fa_value;
+                        mu_element = (-(fa_value / 0.0037) + 182.4)*1e-6;
                     }
 
                     // Speichere den berechneten mu-Wert in den Container
